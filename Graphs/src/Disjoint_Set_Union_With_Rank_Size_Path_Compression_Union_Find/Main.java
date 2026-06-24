a3de577a8b352904f7f2b63e63376b0addeaf555
guruprasadgp22 <guruprasad6200@gmail.com>
2026-06-24 22:21:17 +0530

    2026-06-24 1st

---
+++ Graphs/src/Disjoint_Set_Union_With_Rank_Size_Path_Compression_Union_Find/Main.java
@@ -0,0 +1,5 @@
+package Disjoint_Set_Union_With_Rank_Size_Path_Compression_Union_Find;
+
+public class Main {
+
+}
--- Graphs/src/Prims_Algorithm_Minimum_Spanning_Tree_in_Graph/Main.java
+++ Graphs/src/Prims_Algorithm_Minimum_Spanning_Tree_in_Graph/Main.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 class Pair implements Comparable<Pair>{
+	int u;
 	int v;
 	int w;
-	Pair(int v, int w) {
+	Pair(int u, int v, int w) {
+		this.u = u;
 		this.v = v;
 		this.w = w;
 	}
@@ -22,9 +24,13 @@ class Pair implements Comparable<Pair>{
 public class Main {
 	public static void main(String[] args) {
 		int[][] Edges = {
-				 {0, 1, 5}
+				 {0, 1, 10},
+				 {0, 2, 15},
+				 {0, 3, 30},
+				 {1, 3, 40},
+				 {2, 3, 50}
 		};
-		int v = 2;
+		int v = 4;
 		
 		Main m = new Main();
 		System.out.println("Output: " + m.spanningTree(v, Edges));
@@ -40,27 +46,37 @@ public class Main {
 			int v = edges[i][1];
 			int w = edges[i][2];
 			
-			list.get(u).add(new Pair(v, w));
-			list.get(v).add(new Pair(u, w));
+			list.get(u).add(new Pair(u, v, w));
+			list.get(v).add(new Pair(v, u, w));
 		}
 		
 		HashSet<Integer> set = new HashSet<>();
+		int[][] ans = new int[V][2];
+		int i=0;
 		PriorityQueue<Pair> queue = new PriorityQueue<>();
 	    int minCost = 0;
-		queue.add(new Pair(0, 0));
+		queue.add(new Pair(-1, 0, 0));
 		minCost += 0;
 		while(!queue.isEmpty()) {
 			Pair temp = queue.poll();
 			
 			if(!set.contains(temp.v)) {
 				minCost += temp.w;
-				for(Pair pair: list.get(temp.v)) {
-					queue.add(new Pair(pair.v, pair.w));
-					set.add(temp.v);
+				if(temp.u != -1) {
+					ans[i][0] = temp.u;
+					ans[i][1] = temp.v;
+					i++;
 				}
+				for(Pair pair: list.get(temp.v)) { 
+					queue.add(new Pair(temp.v, pair.v, pair.w));
+				}
+				
+				set.add(temp.v);
 			}
 		}
-		
+		for(int[] ed: ans) {
+			System.out.println(ed[0] + " - " + ed[1]);
+		}
 		return minCost;
 	}
 }
